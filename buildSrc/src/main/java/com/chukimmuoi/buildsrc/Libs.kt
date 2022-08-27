cdd4d107977ab1749a79da415771eb5417e2b1a4
chukimmuoi <chukimmuoi@gmail.com>
2022-08-27 17:04:50 +0700

    Create module buildSrc
    Module have version of mutil libs use by app
    
    Note:
    New import:
    Using multiple catalogs: https://docs.gradle.org/current/userguide/platforms.html#sec:multiple
    libs.versions.toml: https://docs.gradle.org/current/userguide/platforms.html#sub:conventional-dependencies-toml
    
    Tuy nhiên chúng ta không dùng version catalogs trong dự án này.

---
+++ buildSrc/src/main/java/com/chukimmuoi/buildsrc/Libs.kt
@@ -0,0 +1,18 @@
+package com.chukimmuoi.buildsrc
+
+/**
+ * @author: My Project
+ * @Skype: chukimmuoi
+ * @Mobile : +84 167 367 2505
+ * @Email: chukimmuoi@gmail.com
+ * @Website: https://github.com/chukimmuoi
+ * @Project: StockVN
+ * Created by chukimmuoi on 27/08/2022.
+ */
+object Libs {
+    // Default libs support.
+    const val android_application = "com.android.application"
+    const val android_library = "com.android.library"
+    const val kotlin_android = "org.jetbrains.kotlin.android"
+    const val kotlin_jvm = "org.jetbrains.kotlin.jvm"
+}
---
+++ buildSrc/src/main/java/com/chukimmuoi/buildsrc/Versions.kt
@@ -0,0 +1,20 @@
+package com.chukimmuoi.buildsrc
+
+/**
+ * @author: My Project
+ * @Skype: chukimmuoi
+ * @Mobile : +84 167 367 2505
+ * @Email: chukimmuoi@gmail.com
+ * @Website: https://github.com/chukimmuoi
+ * @Project: StockVN
+ * Created by chukimmuoi on 27/08/2022.
+ */
+object Versions {
+    const val versionName = "0.0.1" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+    const val versionCodeBase = 1   // XYYZZM; M = Module (tv, mobile)
+    const val versionCodeMobile = versionCodeBase + 3
+
+    const val compileSdk = 32
+    const val targetSdk = 32
+    const val minSdk = 26
+}
