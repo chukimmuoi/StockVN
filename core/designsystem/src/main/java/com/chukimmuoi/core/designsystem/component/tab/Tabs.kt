9cd974328a105567f7519f8e7d29c25490ca47f1
chukimmuoi <chukimmuoi@gmail.com>
2022-12-04 10:37:06 +0700

    Add tab view
    @see: https://m3.material.io/components/tabs/overview

---
+++ core/designsystem/src/main/java/com/chukimmuoi/core/designsystem/component/tab/Tabs.kt
@@ -0,0 +1,93 @@
+package com.chukimmuoi.core.designsystem.component.tab
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+/**
+ * @author: My Project
+ * @Skype: chukimmuoi
+ * @Mobile : +84 167 367 2505
+ * @Email: chukimmuoi@gmail.com
+ * @Website: https://github.com/chukimmuoi
+ * @Project: StockVN
+ * Created by chukimmuoi on 04/12/2022.
+ */
+/**
+ * Stock tab. Wraps Material 3 [Tab] and shifts text label down.
+ *
+ * @param selected Whether this tab is selected or not.
+ * @param onClick The callback to be invoked when this tab is selected.
+ * @param modifier Modifier to be applied to the tab.
+ * @param enabled Controls the enabled state of the tab. When `false`, this tab will not be
+ * clickable and will appear disabled to accessibility services.
+ * @param text The text label content.
+ */
+@Composable
+fun StockTab(
+    selected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit
+) {
+    Tab(
+        selected = selected,
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        text = {
+            val style = MaterialTheme.typography.labelLarge.copy(textAlign = TextAlign.Center)
+            ProvideTextStyle(
+                value = style,
+                content = {
+                    Box(modifier = Modifier.padding(top = StockTabDefaults.TabTopPadding)) {
+                        text()
+                    }
+                }
+            )
+        }
+    )
+}
+
+
+
+/**
+ * Stock tab row. Wraps Material 3 [TabRow].
+ *
+ * @param selectedTabIndex The index of the currently selected tab.
+ * @param modifier Modifier to be applied to the tab row.
+ * @param tabs The tabs inside this tab row. Typically this will be multiple [StockTab]s. Each element
+ * inside this lambda will be measured and placed evenly across the row, each taking up equal space.
+ */
+@Composable
+fun StockTabRow(
+    selectedTabIndex: Int,
+    modifier: Modifier = Modifier,
+    tabs: @Composable () -> Unit
+) {
+    TabRow(
+        selectedTabIndex = selectedTabIndex,
+        modifier = modifier,
+        containerColor = Color.Transparent,
+        contentColor = MaterialTheme.colorScheme.onSurface,
+        indicator = { tabPositions ->
+            TabRowDefaults.Indicator(
+                modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                height = 2.dp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        },
+        tabs = tabs
+    )
+}
+
+object StockTabDefaults {
+    val TabTopPadding = 7.dp
+}
